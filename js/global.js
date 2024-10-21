968543a2f8fa38fdf905b230f83c45f7dc62362f
BadHunterN1 <144054825+BadHunterN1@users.noreply.github.com>
2024-10-21 22:56:39 +0300

    Update global.js

--- js/global.js
+++ js/global.js
@@ -536,7 +536,7 @@ function initializeToast() {
                 <i class="fas fa-solid fa-check check"></i>
                 <div class="message">
                     <span class="text text-1">Success</span>
-                    <span class="text text-2">Your Cart changes success</span>
+                    <span class="text text-2">Your Cart Has Been Successfully Updated!</span>
                 </div>
             </div>
             <i class="fa-solid fa-xmark close"></i>
